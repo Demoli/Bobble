f66283ba8f1c97c928ef26f8a787779a64fea01b
Neil Aitken <n.aitken@gmail.com>
2019-07-08 07:50:57 +0100

    Shrink bubble collisions boxes
    Show reduce bubbles colliding in uintended positions.

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -7,7 +7,7 @@ onready var bubble_tiles : BubbleTilemap = get_node("/root/Level/BubbleTilemap")
 func _ready():
 	randomize()
 	
-	roof_timer.wait_time = 5
+	roof_timer.wait_time = 30
 	roof_timer.connect("timeout", self, "lower_roof")
 	add_child(roof_timer)
 	roof_timer.start()
