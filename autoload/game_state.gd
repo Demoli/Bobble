cf50d9d3cc464bd8de0fdb8e3df194931f10f712
Neil Aitken <n.aitken@gmail.com>
2019-07-08 18:35:10 +0100

    you died

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -7,7 +7,7 @@ onready var bubble_tiles : BubbleTilemap = get_node("/root/Level/BubbleTilemap")
 func _ready():
 	randomize()
 	
-	roof_timer.wait_time = 30
+	roof_timer.wait_time = 1
 	roof_timer.connect("timeout", self, "lower_roof")
 	add_child(roof_timer)
 	roof_timer.start()
@@ -18,3 +18,7 @@ func _ready():
 func lower_roof():
 	roof.position.y += 16
 	bubble_tiles.move_down()
+	
+func game_over():
+	roof_timer.stop()
+	get_tree().change_scene("res://scenes/GameOver.tscn")
